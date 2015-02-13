6bcffbcb67fdf1665b69c0071b99577f383bb091
Michael Schmitz <michael@schmitztech.com>
2015-02-13 08:48:54 -0800

    Add main method for SampleExperiment.

--- src/test/scala/org/allenai/pipeline/SampleExperiment.scala
+++ src/test/scala/org/allenai/pipeline/SampleExperiment.scala
@@ -9,118 +9,13 @@ import spray.json.DefaultJsonProtocol._
 
 import scala.util.Random
 
-import java.io.{ InputStream, File }
+import java.io.{ InputStream, File, PrintWriter }
 
 /** Test PipelineRunner functionality */
 class SampleExperiment extends UnitSpec
     with BeforeAndAfterEach with BeforeAndAfterAll with ScratchDirectory {
 
-  case class TrainedModel(info: String)
-
-  object TrainedModel {
-    val jsonFormat = jsonFormat1(apply)
-  }
-
-  type TrainingPoint = (Boolean, Array[Double])
-
-  class JoinAndSplitData(
-    features: Producer[Iterable[Array[Double]]],
-    labels: Producer[Iterable[Boolean]],
-    testSizeRatio: Double
-  )
-      extends Producer[(Iterable[TrainingPoint], Iterable[TrainingPoint])] with Ai2CodeInfo {
-    def create: (Iterable[TrainingPoint], Iterable[TrainingPoint]) = {
-      val rand = new Random
-      val data = labels.get.zip(features.get)
-      val testSize = math.round(testSizeRatio * data.size).toInt
-      (data.drop(testSize), data.take(testSize))
-    }
-
-    override def signature: Signature =
-      Signature.fromFields(this, "features", "labels", "testSizeRatio")
-  }
-
-  case class TrainModel(trainingData: Producer[Iterable[TrainingPoint]])
-      extends Producer[TrainedModel] with Ai2Signature {
-    def create: TrainedModel = {
-      val dataRows = trainingData.get
-      train(dataRows) // Run training algorithm on training data
-    }
-
-    def train(data: Iterable[TrainingPoint]): TrainedModel =
-      TrainedModel(s"Trained model with ${data.size} rows")
-
-    override def signature = Signature.fromObject(this)
-  }
-
-  type PRMeasurement = Iterable[(Double, Double, Double)]
-
-  // Threshold, precision, recall
-  case class MeasureModel(
-    val model: Producer[TrainedModel],
-    val testData: Producer[Iterable[TrainingPoint]]
-  )
-      extends Producer[PRMeasurement] with Ai2Signature {
-    def create: PRMeasurement = {
-      model.get
-      // Just generate some dummy data
-      val rand = new Random
-      import math.exp
-      var a = 0.0
-      var b = 0.0
-      for (i <- (0 until testData.get.size)) yield {
-        val r = (exp(-a), 1 - exp(-b), exp(-b))
-        a += rand.nextDouble * .03
-        b += rand.nextDouble * .03
-        r
-      }
-    }
-  }
-
-  case class ParsedDocument(info: String)
-
-  case class FeaturizeDocuments(documents: Producer[Iterator[ParsedDocument]])
-      extends Producer[Iterable[Array[Double]]] with Ai2Signature {
-    def create: Iterable[Array[Double]] = {
-      val features = for (doc <- documents.get) yield {
-        val rand = new Random
-        Array.fill(8)(rand.nextDouble) // scalastyle:ignore
-      }
-      features.toList
-    }
-  }
-
-  object ParseDocumentsFromXML extends ArtifactIo[Iterator[ParsedDocument], StructuredArtifact]
-      with Ai2CodeInfo {
-    def read(a: StructuredArtifact): Iterator[ParsedDocument] = {
-      for ((id, is) <- a.reader.readAll) yield parse(id, is)
-    }
-
-    def parse(id: String, is: InputStream): ParsedDocument = ParsedDocument(id)
-
-    // Writing back to XML not supported
-    def write(data: Iterator[ParsedDocument], artifact: StructuredArtifact): Unit = ???
-
-    override def toString: String = this.getClass.getSimpleName
-  }
-
-  class TrainModelPython(val data: Producer[FlatArtifact], val io: ArtifactIo[TrainedModel, FileArtifact])
-      extends Producer[TrainedModel] with Ai2CodeInfo {
-    def create: TrainedModel = {
-      val inputFile = File.createTempFile("trainData", ".tsv")
-      val outputFile = File.createTempFile("model", ".json")
-      data.get.copyTo(new FileArtifact(inputFile))
-      import sys.process._
-      import scala.language.postfixOps
-      // In real world, omit "echo"
-      val stdout: String = s"echo train.py -input $inputFile -output $outputFile" !!
-      // val model = io.read(new FileArtifact(outputFile)) // In real world, return this
-      val model = TrainedModel(stdout)
-      model
-    }
-
-    override def signature: Signature = Signature.fromFields(this, "data", "io")
-  }
+  import SampleExperiment._
 
   val inputDir = new File("src/test/resources/pipeline")
   val featureFile = "features.txt"
@@ -234,4 +129,165 @@ class SampleExperiment extends UnitSpec
   def findFile(dir: File, prefix: String, suffix: String): Boolean =
     dir.listFiles.map(_.getName).exists(s => s.startsWith(prefix) && s.endsWith(suffix))
 
+}
+
+object SampleExperiment {
+  case class TrainedModel(info: String)
+
+  object TrainedModel {
+    val jsonFormat = jsonFormat1(apply)
+  }
+
+  type TrainingPoint = (Boolean, Array[Double])
+
+  class JoinAndSplitData(
+    features: Producer[Iterable[Array[Double]]],
+    labels: Producer[Iterable[Boolean]],
+    testSizeRatio: Double
+  )
+      extends Producer[(Iterable[TrainingPoint], Iterable[TrainingPoint])] with Ai2CodeInfo {
+    def create: (Iterable[TrainingPoint], Iterable[TrainingPoint]) = {
+      val rand = new Random
+      val data = labels.get.zip(features.get)
+      val testSize = math.round(testSizeRatio * data.size).toInt
+      (data.drop(testSize), data.take(testSize))
+    }
+
+    override def signature: Signature =
+      Signature.fromFields(this, "features", "labels", "testSizeRatio")
+  }
+
+  case class TrainModel(trainingData: Producer[Iterable[TrainingPoint]])
+      extends Producer[TrainedModel] with Ai2Signature {
+    def create: TrainedModel = {
+      val dataRows = trainingData.get
+      train(dataRows) // Run training algorithm on training data
+    }
+
+    def train(data: Iterable[TrainingPoint]): TrainedModel =
+      TrainedModel(s"Trained model with ${data.size} rows")
+
+    override def signature = Signature.fromObject(this)
+  }
+
+  type PRMeasurement = Iterable[(Double, Double, Double)]
+
+  // Threshold, precision, recall
+  case class MeasureModel(
+    val model: Producer[TrainedModel],
+    val testData: Producer[Iterable[TrainingPoint]]
+  )
+      extends Producer[PRMeasurement] with Ai2Signature {
+    def create: PRMeasurement = {
+      model.get
+      // Just generate some dummy data
+      val rand = new Random
+      import math.exp
+      var a = 0.0
+      var b = 0.0
+      for (i <- (0 until testData.get.size)) yield {
+        val r = (exp(-a), 1 - exp(-b), exp(-b))
+        a += rand.nextDouble * .03
+        b += rand.nextDouble * .03
+        r
+      }
+    }
+  }
+
+  case class ParsedDocument(info: String)
+
+  case class FeaturizeDocuments(documents: Producer[Iterator[ParsedDocument]])
+      extends Producer[Iterable[Array[Double]]] with Ai2Signature {
+    def create: Iterable[Array[Double]] = {
+      val features = for (doc <- documents.get) yield {
+        val rand = new Random
+        Array.fill(8)(rand.nextDouble) // scalastyle:ignore
+      }
+      features.toList
+    }
+  }
+
+  object ParseDocumentsFromXML extends ArtifactIo[Iterator[ParsedDocument], StructuredArtifact]
+      with Ai2CodeInfo {
+    def read(a: StructuredArtifact): Iterator[ParsedDocument] = {
+      for ((id, is) <- a.reader.readAll) yield parse(id, is)
+    }
+
+    def parse(id: String, is: InputStream): ParsedDocument = ParsedDocument(id)
+
+    // Writing back to XML not supported
+    def write(data: Iterator[ParsedDocument], artifact: StructuredArtifact): Unit = ???
+
+    override def toString: String = this.getClass.getSimpleName
+  }
+
+  class TrainModelPython(val data: Producer[FlatArtifact], val io: ArtifactIo[TrainedModel, FileArtifact])
+      extends Producer[TrainedModel] with Ai2CodeInfo {
+    def create: TrainedModel = {
+      val inputFile = File.createTempFile("trainData", ".tsv")
+      val outputFile = File.createTempFile("model", ".json")
+      data.get.copyTo(new FileArtifact(inputFile))
+      import sys.process._
+      import scala.language.postfixOps
+      // In real world, omit "echo"
+      val stdout: String = s"echo train.py -input $inputFile -output $outputFile" !!
+      // val model = io.read(new FileArtifact(outputFile)) // In real world, return this
+      val model = TrainedModel(stdout)
+      model
+    }
+
+    override def signature: Signature = Signature.fromFields(this, "data", "io")
+  }
+}
+
+/** An application that write out pipeline files for human viewing. */
+object SampleExperimentApp extends App {
+  import SampleExperiment._
+
+  val inputDir = new File("src/test/resources/pipeline")
+  val featureFile = "features.txt"
+  val labelFile = "labels.txt"
+
+  // Enable JSON serialization for our trained model object
+
+  import org.allenai.pipeline.IoHelpers._
+
+  implicit val modelFormat = TrainedModel.jsonFormat
+
+  implicit val prMeasurementFormat: StringSerializable[(Double, Double, Double)] =
+    tuple3ColumnFormat[Double, Double, Double](',')
+
+  // Define our persistence implementation
+
+  val input = new RelativeFileSystem(inputDir)
+  val scratchDir = new File(".")
+
+  // TSV format for label+features is <label><tab><comma-separated feature values>
+  implicit val featureFormat = columnArrayFormat[Double](',')
+  implicit val labelFeatureFormat = tuple2ColumnFormat[Boolean, Array[Double]]('\t')
+
+  implicit val runner = PipelineRunner.writeToDirectory(scratchDir)
+
+  val docDir = new DirectoryArtifact(new File(inputDir, "xml"))
+  val docs = Read.fromArtifact(ParseDocumentsFromXML, docDir)
+  val docFeatures = new FeaturizeDocuments(docs)
+
+  // Define pipeline
+  val labelData: Producer[Iterable[Boolean]] =
+    Read.Collection.fromText[Boolean](input.flatArtifact(labelFile))
+  val Producer2(trainData, testData) = new JoinAndSplitData(docFeatures, labelData, 0.2)
+  val trainDataPersisted = Persist.Collection.asText(trainData)
+  val model = Persist.Singleton.asJson(new TrainModel(trainDataPersisted))
+  val measure: Producer[PRMeasurement] = Persist.Collection.asText(
+    new MeasureModel(model, testData)
+  )
+  runner.run(measure)
+
+  val outputFile = new File("diagram.html")
+  val pw = new PrintWriter(outputFile)
+  val html = Workflow.renderHtml(Workflow.forPipeline(measure))
+  pw.write(html)
+  pw.close()
+
+  println("Pipeline diagram written to " + outputFile.getAbsolutePath)
 }
