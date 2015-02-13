84b1424c1af44b1f8b27aa6e5ad759ee78db8471
Michael Schmitz <michael@schmitztech.com>
2015-02-12 17:05:59 -0800

    Color source and sink nodes.

--- src/main/scala/org/allenai/pipeline/Workflow.scala
+++ src/main/scala/org/allenai/pipeline/Workflow.scala
@@ -11,7 +11,15 @@ import java.net.URI
 
 /** DAG representation of the execution of a set of Producers.
   */
-case class Workflow(nodes: Map[String, Node], links: Iterable[Link])
+case class Workflow(nodes: Map[String, Node], links: Iterable[Link]) {
+  def sourceNodes() = nodes.filter { case (nodeId, node) => 
+    !links.exists(link => link.toId == nodeId)
+  }
+
+  def sinkNodes() = nodes.filter { case (nodeId, node) => 
+    !links.exists(link => link.fromId == nodeId)
+  }
+}
 
 /** Represents a Producer instance with PipelineRunnerSupport */
 case class Node(
@@ -76,6 +84,8 @@ object Workflow {
     }
 
   def renderHtml(w: Workflow): String = {
+    val sourceNodes = w.sourceNodes()
+    val sinkNodes = w.sinkNodes()
     val addNodes = 
       for ((id, Node(info, params, outputPath)) <- w.nodes) yield {
         // Params show up as line items in the pipeline diagram node.
@@ -89,8 +99,14 @@ object Workflow {
           info.srcUrl.map(uri => s"""new Link("${link(uri)}","v${info.buildId}")""") ++
           // An optional link to the output data.
           outputPath.map(uri => s"""new Link("${link(uri)}","output")""")
+        val clazz = sourceNodes match {
+          case _ if sourceNodes contains id => "sourceNode"
+          case _ if sinkNodes contains id => "sinkNode"
+          case _ => ""
+        }
         val linksText = links.mkString(",")
         s"""        g.setNode("$id", {
+           |          class: "$clazz",
            |          labelType: "html",
            |          label: generateStepContent("${info.className}",
            |            [$paramsText],
