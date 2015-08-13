2883e53c98e7852827f86c53be345a17392a75b1
rodneykinney <rodneyk@allenai.org>
2015-08-13 13:59:25 -0700

    KeyValueIo.read

--- src/main/scala/org/allenai/pipeline/hackathon/KeyValueIo.scala
+++ src/main/scala/org/allenai/pipeline/hackathon/KeyValueIo.scala
@@ -1,5 +1,9 @@
 package org.allenai.pipeline.hackathon
+
+import org.allenai.common.Resource
 import org.allenai.pipeline._
+
+import scala.io.Source
 
 object KeyValueIo extends ArtifactIo[Map[String, String], StructuredArtifact] {
   override def write(data: Map[String, String], artifact: StructuredArtifact): Unit = {
@@ -12,7 +16,11 @@ object KeyValueIo extends ArtifactIo[Map[String, String], StructuredArtifact] {
     }
   }
 
-  override def read(artifact: StructuredArtifact): Map[String, String] = ???
+  override def read(artifact: StructuredArtifact): Map[String, String] = {
+    (for ((name, is) <- artifact.reader.readAll) yield {
+      (name, Resource.using(is) { i => Source.fromInputStream(i).mkString })
+    }).toMap
+  }
 
   override def stepInfo: PipelineStepInfo = PipelineStepInfo("KeyValueIo")
 }
